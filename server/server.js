5c454dc1ce436b1497a513f88f8fb1590305e330
karandeshmukh11 <karandeshmukhkd26@gmail.com>
2026-02-24 15:02:25 +0530

    cors fixed

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const allowedorigins = [
 "http://localhost:5173",
 "http://localhost:5174",
 'https://righ-verse.vercel.app',
-'https://righ-verse-e4ql.vercel.app/'
+'https://righ-verse-e4ql.vercel.app'
 
 ]
 
